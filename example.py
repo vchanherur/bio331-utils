425fbfdc7c7ba05b966979d346adf5ec69f4755c
Vikram Chan-Herur <vch@c099h083.acad.reed.edu>
2016-10-25 22:02:45 -0700

    finished example.py

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 # Here's a testing file for the graph and file tools.
 
+from file_tools import *
+from graph_tools import *
+
 # testing readin
 n,e,w = readin("test.txt",True)
 
@@ -11,4 +14,14 @@ print(node_degree("b",n,e,"in")
 print(node_degree("b",n,e,"out")
 print(node_degree("b",n,e)
 
-#  
+# testing bfs
+print(bfs(n,e,"A")
+
+# testing lcc
+print(lcc(n,e))
+
+# testing random node
+print(random_node(n))
+
+# testing random edge
+print(random_edge(e))
